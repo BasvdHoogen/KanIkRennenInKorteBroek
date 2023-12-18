72c554ab124df47f947c5a91c4ab3ef124e254a6
Bas van den Hoogen <bas.van.den.hoogen@avivasolutions.nl>
2023-12-18 13:53:40 +0100

    Second try

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,4 @@
 <script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
 import {ref} from 'vue'
 
 const weather = ref(null)
@@ -10,13 +8,13 @@ fetch("https://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam").
 </script>
 
 <template>
-  <div v-if="weatherData > 8">
+  <div v-if="weatherData != null && weatherData > 8">
     <h3>JA!</h3>
-    Trek je korte broek aan. Het is {{ weatherData }} graden!
+    Trek je KORTE broek aan. Het is {{ weatherData }} °C
   </div>
-  <div v-else-if="weatherData < 8">
+  <div v-else-if="weatherData != null && weatherData < 8">
     <h3>Nee!</h3>
-    Trek je LANGE broek aan. Het is {{ weatherData }} graden!
+    Trek je LANGE broek aan. Het is {{ weatherData }} °C
   </div>
   <div v-else>
     <h3>Helaas...</h3>
@@ -29,6 +27,5 @@ h3 {
   font-size: 1.2rem;
   font-weight: 500;
   margin-bottom: 0.4rem;
-  color: var(--color-heading);
 }
 </style>
