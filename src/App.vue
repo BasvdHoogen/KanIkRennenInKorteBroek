19224e5837eadcc6abebcd1b95c022d8e85e395a
Bas van den Hoogen <bas.van.den.hoogen@avivasolutions.nl>
2023-12-18 13:24:33 +0100

    first try

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="green">In korte broek rennen?</h1>
+    <TheWelcome />
+  </main>
+</template>
+
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,34 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import {ref} from 'vue'
+
+const weather = ref(null)
+const weatherData = ref(null)
+fetch("https://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam").then((r) => r.json()).then(data => weatherData.value = data.liveweer[0].temp)
+
+</script>
+
+<template>
+  <div v-if="weatherData > 8">
+    <h3>JA!</h3>
+    Trek je korte broek aan. Het is {{ weatherData }} graden!
+  </div>
+  <div v-else-if="weatherData < 8">
+    <h3>Nee!</h3>
+    Trek je LANGE broek aan. Het is {{ weatherData }} graden!
+  </div>
+  <div v-else>
+    <h3>Helaas...</h3>
+    Er is iets mis gegaan
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+</style>
