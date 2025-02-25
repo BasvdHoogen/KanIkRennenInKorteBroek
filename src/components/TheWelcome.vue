352741f70364d4f3877eeec09b19efd361ee4694
bas.vandenhoogen <bas.vandenhoogen@greenchoice.nl>
2025-02-25 22:19:28 +0100

    grid

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -27,8 +27,6 @@ watch(location, () => {
 })
 
 function GetWeather() {
-
-
   try{
       fetch(fullFetchUri.value)
           .then((r) => r.json())
@@ -61,39 +59,39 @@ function checkIfEnter(event: KeyboardEvent) {
 </script>
 
 <template>
-  <div style="min-height: 80lvh">
-    <h1 class="green">Kan ik in korte broek rennen?</h1>
-    <br />
-    <div><img src="/man-running-emoji-258749.png" alt="man-running-emoji-with-short-pants"
+  <div style="min-height: 90vh">
+        <div><img src="/man-running-emoji-258749.png" alt="man-running-emoji-with-short-pants"
               style="width:128px;height:128px;display: flex; margin-left: auto; margin-right: auto;"></div>
     <br />
     <input type="text" v-model="locationInput" placeholder="Zoek een locatie" v-on:keyup="checkIfEnter" /> <button @click="RedirectToLocationUri">Zoek</button>
     <br /><br />
+
+    <h1 class="green">Kan ik in korte broek rennen?</h1>
 
     <loading-wave v-if="loading" />
     <div v-else>
       <div v-if="successful == true && weatherData != null">
           <div v-if="weatherData.current != null && weatherData.current.apparent_temperature >= 8">
             <h3>JA!</h3>
-            Trek je KORTE broek aan. De gevoelstemperatuur is {{ weatherData.current.apparent_temperature }} °C
+            Trek je KORTE broek aan.
           </div>
           <div v-else>
             <h3>Nee!</h3>
-            Trek je LANGE broek aan. De gevoelstemperatuur is {{ weatherData.current.apparent_temperature }} °C
+            Trek je LANGE broek aan.
           </div>
 
           <br>
-          <br>
           <div v-if="weatherData.current != null">
-            <h4>Het weer
-              <span v-if="weatherData.displayName">in: <b>{{ weatherData.displayName }}</b> </span>
-              <span v-else>op coördinaat: {{ weatherData.latitude }}, {{ weatherData.longitude }}</span>
+            <h4>
+              <span v-if="weatherData.displayName"><b>{{ weatherData.displayName }}</b> </span>
+              <span v-else> {{ weatherData.latitude }}, {{ weatherData.longitude }}</span>
             </h4>
-            <br>
-            Temperatuur: {{ weatherData.current.temperature_2m }} {{weatherData.current_units.temperature_2m}}<br>
-            Gevoelstemperatuur: {{ weatherData.current.apparent_temperature }} {{weatherData.current_units.apparent_temperature}}<br>
-            Wind: {{weatherData.current.windspeed_10m}} {{weatherData.current_units.windspeed_10m}}<br>
-            Beschrijving: {{weatherData.current.weathercodeString}}
+            <div class="grid">
+              <div class="right">Temperatuur: </div><div class="left">{{ weatherData.current.temperature_2m }} {{weatherData.current_units.temperature_2m}}</div>
+              <div class="right">Gevoelstemperatuur: </div><div class="left">{{ weatherData.current.apparent_temperature }} {{weatherData.current_units.apparent_temperature}}</div>
+              <div class="right">Wind: </div><div class="left">{{weatherData.current.windspeed_10m}} {{weatherData.current_units.windspeed_10m}}</div>
+              <div class="right">Beschrijving: </div><div class="left">{{weatherData.current.weathercodeString}}</div>
+            </div>
           </div>
       </div>
       <div v-else>
@@ -121,5 +119,19 @@ h6 {
   font-size: 0.7rem;
   font-weight: 400;
   margin-bottom: 0.4rem;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 0.5em;
+}
+
+.left {
+  text-align: left;
+}
+
+.right {
+  text-align: right;
 }
 </style>
